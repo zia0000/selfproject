dd4e8e71fb4903be8daaba8fbfefbc582c8bb61e
Gia <kgkim774@gmail.com>
2022-05-21 18:42:52 +0900

    commit3

--- src/components/Answers.tsx
+++ src/components/Answers.tsx
@@ -5,44 +5,38 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store";
 
 function Answer() {
-  const dataone = {
-    title: "answer2",
-    content:
-      "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
-    image: "image/resultdummysmall1.jpg",
-  };
-  // const data = [
-  //   {
-  //     title: "answer2",
-  //     content:
-  //       "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
-  //     image: "image/resultdummysmall1.jpg",
-  //   },
-  //   {
-  //     title: "answer3",
-  //     content:
-  //       "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
-  //     image: "image/resultdummysmall1.jpg",
-  //   },
-  //   {
-  //     title: "answer4",
-  //     content:
-  //       "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
-  //     image: "image/resultdummysmall1.jpg",
-  //   },
-  //   {
-  //     title: "answer5",
-  //     content:
-  //       "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
-  //     image: "image/resultdummysmall1.jpg",
-  //   },
-  //   {
-  //     title: "answer6",
-  //     content:
-  //       "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
-  //     image: "image/resultdummysmall1.jpg",
-  //   },
-  // ];
+  const data = [
+    {
+      title: "answer2",
+      content:
+        "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
+      image: "image/resultdummysmall1.jpg",
+    },
+    {
+      title: "answer3",
+      content:
+        "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
+      image: "image/resultdummysmall1.jpg",
+    },
+    {
+      title: "answer4",
+      content:
+        "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
+      image: "image/resultdummysmall1.jpg",
+    },
+    {
+      title: "answer5",
+      content:
+        "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
+      image: "image/resultdummysmall1.jpg",
+    },
+    {
+      title: "answer6",
+      content:
+        "계절이 지나가는 하늘에는 가을로 가득 차 있습니다. 나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다. 가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요,내일 밤이 남은 까닭이요,아직 나의 청춘이 다하지 않은 까닭입니다.",
+      image: "image/resultdummysmall1.jpg",
+    },
+  ];
 
   const [sum, setSum] = useState(0);
 
@@ -51,7 +45,8 @@ function Answer() {
 
   return (
     <div>
-      {dataone.slice(1).map((item) => (
+      if (score = 12);
+      {data.slice(0, 1).map((item) => (
         <React.Fragment key={item.title}>
           <h1>{item.title}</h1>
           <article>{item.content}</article>
